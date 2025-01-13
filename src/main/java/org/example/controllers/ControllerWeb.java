27e79e240db3705dfa9ce329622d1dd0ba74b3b7
Bat14 <batyaultr03@gmail.com>
2025-01-13 23:02:45 +0400

    Endpoint Добвлен root-endpoint и endpoint каталога.

--- src/main/java/org/example/controllers/ControllerWeb.java
+++ src/main/java/org/example/controllers/ControllerWeb.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class ControllerWeb {
     @RequestMapping(value = "catalog/", method = RequestMethod.GET)
-    @Operation(summary = "Example endpoint", description = "This is a sample endpoint.")
+    @Operation(summary = "Catalog endpoint", description = "This is a catalog endpoint.")
     public String showCatalog() {
         return "catalog-view";
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    @Operation(summary = "Example endpoint", description = "This is a sample endpoint.")
+    @Operation(summary = "Root endpoint", description = "This is a root endpoint.")
     public String showHomePage() {
         return "catalog-view";
     }
